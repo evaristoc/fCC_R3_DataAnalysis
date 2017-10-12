bfa55e5125331e897793a79dee0f79fff9e0d88b
ec <varikvi@yahoo.com>
2017-10-12 16:55:00 +0200

    coming from issue #21

--- checkin-the-classifier/extract_urls.py
+++ checkin-the-classifier/extract_urls.py
@@ -1,3 +1,4 @@
+import os,sys
 import csv
 from collections import defaultdict
 import pprint
@@ -6,7 +7,7 @@ import re
 
 fcc_hostnames = []
 ukwa_hostnames = []
-with open('good_rows_fcc.csv', 'r') as data:
+with open(os.getcwd()+'/checkin-the-classifier/good_rows_fcc.csv', 'r') as data:
     reader = csv.reader(data, delimiter=',')
     url_row = 2
     next(reader)
@@ -17,9 +18,10 @@ with open('good_rows_fcc.csv', 'r') as data:
         hostname = hostname.replace('.io',"")
         hostname = hostname.replace('.net',"")
         hostname = hostname.replace('.co',"")
-        fcc_hostnames.append(hostname)
+        #fcc_hostnames.append(hostname)
+        fcc_hostnames.append(hostname.split("."))
 
-with open('classification_UKWA.tsv', 'r') as data:
+with open(os.getcwd()+'/checkin-the-classifier/classification_UKWA.tsv', 'r') as data:
     reader = csv.reader(data, delimiter='\t')
     url_row = 3
     for row in reader:
@@ -36,4 +38,11 @@ with open('classification_UKWA.tsv', 'r') as data:
         hostname = hostname.replace('.uk/',"")
         ukwa_hostnames.append(hostname)
 
-print("Same hostnames:", list(set(fcc_hostnames) & set(ukwa_hostnames)))
+#print("Same hostnames:", list(set(fcc_hostnames) & set(ukwa_hostnames)))
+
+for fcch in fcc_hostnames:
+    for sec in fcch:
+        for ukwah in ukwa_hostnames:
+            if ukwah.find(sec) != -1:
+                if len(ukwah) == len(sec):
+                    print(sec, ukwah)
