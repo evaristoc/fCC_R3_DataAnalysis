0989b5adb996f74897ebdb944c9826d8ece841b5
deborah <deh.mesquita@hotmail.com>
2017-10-18 09:00:41 -0300

    Extract labels from learn-anything

---
+++ checking-the-classifier/extract_labels.py
@@ -0,0 +1,49 @@
+import csv
+from collections import defaultdict
+import pprint
+import json
+
+def extractLabelsUKWA(dir):
+    with open(dir, 'r') as data:
+        reader = csv.reader(data, delimiter='\t')
+        categories = {}
+        next(reader)
+        for row in reader:
+            if row[0] not in categories:
+                categories[row[0]] = 1
+            else:
+                categories[row[0]] += 1
+
+        pprint.pprint(categories)
+        output_dict = []
+        for category,size in categories.items():
+            output_dict.append({"size":size,"name":category})
+
+        with open("categories_UKWA_teste.json", "w") as fp:
+            json.dump(output_dict, fp)
+
+
+def extractLabels(file_dir, output_dir, category_row_id, delimiter):
+    with open(file_dir, 'r') as data:
+        reader = csv.reader(data, delimiter=delimiter)
+        categories = {}
+        next(reader)
+        for row in reader:
+            if row[category_row_id] not in categories:
+                categories[row[category_row_id]] = 1
+            else:
+                categories[row[category_row_id]] += 1
+        pprint.pprint(categories)
+
+        output_dict = []
+        for category,size in categories.items():
+            output_dict.append({"size":size,"name":category.capitalize()})
+
+        with open(output_dir, "w") as fp:
+            json.dump(output_dict, fp)
+
+
+if __name__ == '__main__':
+    extractLabelsUKWA('classification_UKWA.tsv')
+    extractLabels('category_operationalization.csv', 'categories_fcc.json',0,":")
+    extractLabels('../learn_anything_urls/learn-anything_urls.csv', 'categories_learn_anything.json',1,",")
